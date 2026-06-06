50fa054cc162dccdc3c2f8aec5249fc931a8b1a5
GgVox <danielacostadiaz0@gmail.com>
2026-06-06 17:22:31 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+async function saveScript() {
+
+    const code = document.getElementById("code").value;
+
+    const response = await fetch(
+        "https://TU-API.com/save",
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                code:code
+            })
+        }
+    );
+
+    const data = await response.json();
+
+    document.getElementById("result").innerHTML =
+        location.origin + "/view.html?id=" + data.id;
+}
