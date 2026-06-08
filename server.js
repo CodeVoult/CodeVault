75bd8823c1dc821aabbeb164f56aec52af049b21
ZyroxVault <danielacostadiaz0@gmail.com>
2026-06-08 18:50:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ const axios = require("axios");
 const crypto = require("crypto");
 
 const app = express();
+
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({
+    limit: "100mb"
+}));
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "100mb"
+}));
 
 const REALTIME_DB_URL = "https://codevault-9ca85-default-rtdb.firebaseio.com/scripts";
 
